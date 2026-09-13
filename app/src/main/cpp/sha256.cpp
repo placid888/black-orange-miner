66ca516e731d065ef3fc269609a4ae8aa921317f
placid888 <placid888@gmail.com>
2026-09-14 06:45:31 +0800

    update

--- app/src/main/cpp/sha256.cpp
+++ app/src/main/cpp/sha256.cpp
@@ -1,115 +1,105 @@
 #include "sha256.h"
+#include <arm_neon.h>
 #include <string.h>
 
-#if defined(__ARM_FEATURE_CRYPTO)
-#include <arm_neon.h>
-#endif
-
-#define ROTLEFT(a,b) (((a) << (b)) | ((a) >> (32-(b))))
-#define ROTRIGHT(a,b) (((a) >> (b)) | ((a) << (32-(b))))
-#define CH(x,y,z) (((x) & (y)) ^ (~(x) & (z)))
-#define MAJ(x,y,z) (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
-#define EP0(x) (ROTRIGHT(x,2) ^ ROTRIGHT(x,13) ^ ROTRIGHT(x,22))
-#define EP1(x) (ROTRIGHT(x,6) ^ ROTRIGHT(x,11) ^ ROTRIGHT(x,25))
-#define SIG0(x) (ROTRIGHT(x,7) ^ ROTRIGHT(x,18) ^ ((x) >> 3))
-#define SIG1(x) (ROTRIGHT(x,17) ^ ROTRIGHT(x,19) ^ ((x) >> 10))
-
-static const uint32_t k[64] = {
-    0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,
-    0xd807aa98,0x12835b01,0x243185be,0x550c7dc3,0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,
-    0xe49b69c1,0xefbe4786,0x0fc19dc6,0x240ca1cc,0x2de92c6f,0x4a7484aa,0x5cb0a9dc,0x76f988da,
-    0x983e5152,0xa831c66d,0xb00327c8,0xbf597fc7,0xc6e00bf3,0xd5a79147,0x06ca6351,0x14292967,
-    0x27b70a85,0x2e1b2138,0x4d2c6dfc,0x53380d13,0x650a7354,0x766a0abb,0x81c2c92e,0x92722c85,
-    0xa2bfe8a1,0xa81a664b,0xc24b8b70,0xc76c51a3,0xd192e819,0xd6990624,0xf40e3585,0x106aa070,
-    0x19a4c116,0x1e376c08,0x2748774c,0x34b0bcb5,0x391c0cb3,0x4ed8aa4a,0x5b9cca4f,0x682e6ff3,
-    0x748f82ee,0x78a5636f,0x84c87814,0x8cc70208,0x90befffa,0xa4506ceb,0xbef9a3f7,0xc67178f2
+static const uint32_t K[64] = {
+    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
+    0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+    0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
+    0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
+    0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+    0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
+    0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
+    0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+    0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
 };
 
-#if defined(__ARM_FEATURE_CRYPTO)
-// ARMv8 硬體加速實作
-static void sha256_transform_neon(SHA256_CTX *ctx, const uint8_t data[]) {
-    uint32x4_t state0 = vld1q_u32(&ctx->state[0]); 
-    uint32x4_t state1 = vld1q_u32(&ctx->state[4]); 
+static void sha256_transform_armv8(uint32_t* state, const uint8_t* data) {
+    uint32x4_t msg[4];
+    msg[0] = vreinterpretq_u32_u8(vrev32q_u8(vld1q_u8(data)));
+    msg[1] = vreinterpretq_u32_u8(vrev32q_u8(vld1q_u8(data + 16)));
+    msg[2] = vreinterpretq_u32_u8(vrev32q_u8(vld1q_u8(data + 32)));
+    msg[3] = vreinterpretq_u32_u8(vrev32q_u8(vld1q_u8(data + 48)));
 
-    // 載入區塊資料並反轉位元組 (Big-Endian 轉換)
-    uint32x4_t msg0 = vreinterpretq_u32_u8(vrev32q_u8(vld1q_u8(data)));
-    uint32x4_t msg1 = vreinterpretq_u32_u8(vrev32q_u8(vld1q_u8(data + 16)));
-    uint32x4_t msg2 = vreinterpretq_u32_u8(vrev32q_u8(vld1q_u8(data + 32)));
-    uint32x4_t msg3 = vreinterpretq_u32_u8(vrev32q_u8(vld1q_u8(data + 48)));
+    uint32x4_t abcd = vld1q_u32(state);
+    uint32x4_t efgh = vld1q_u32(state + 4);
+    uint32x4_t abcd_save = abcd;
+    uint32x4_t efgh_save = efgh;
 
-    uint32x4_t tmp;
+    for (int i = 0; i < 64; i += 16) {
+        uint32x4_t wk, temp_abcd, temp_efgh;
 
-    // 定義硬體運算巨集，呼叫 CPU 的專用加密電路
-    #define SHA256_ROUND(w_val, k_idx) \
-        tmp = state0; \
-        state0 = vsha256hq_u32(state0, state1, vaddq_u32(w_val, vld1q_u32(&k[k_idx]))); \
-        state1 = vsha256h2q_u32(state1, tmp, vaddq_u32(w_val, vld1q_u32(&k[k_idx])));
+        wk = vaddq_u32(msg[0], vld1q_u32(&K[i]));
+        temp_abcd = vsha256hq_u32(abcd, efgh, wk);
+        temp_efgh = vsha256h2q_u32(efgh, abcd, wk);
+        abcd = temp_abcd; efgh = temp_efgh;
 
-    #define SHA256_MSG_UPDATE(w0, w1, w2, w3) \
-        vsha256su1q_u32(vsha256su0q_u32(w0, w1), w2, w3)
+        wk = vaddq_u32(msg[1], vld1q_u32(&K[i + 4]));
+        temp_abcd = vsha256hq_u32(abcd, efgh, wk);
+        temp_efgh = vsha256h2q_u32(efgh, abcd, wk);
+        abcd = temp_abcd; efgh = temp_efgh;
 
-    SHA256_ROUND(msg0, 0);
-    SHA256_ROUND(msg1, 4);
-    SHA256_ROUND(msg2, 8);
-    SHA256_ROUND(msg3, 12);
+        wk = vaddq_u32(msg[2], vld1q_u32(&K[i + 8]));
+        temp_abcd = vsha256hq_u32(abcd, efgh, wk);
+        temp_efgh = vsha256h2q_u32(efgh, abcd, wk);
+        abcd = temp_abcd; efgh = temp_efgh;
 
-    for (int i = 16; i < 64; i += 16) {
-        msg0 = SHA256_MSG_UPDATE(msg0, msg1, msg2, msg3);
-        SHA256_ROUND(msg0, i);
-        msg1 = SHA256_MSG_UPDATE(msg1, msg2, msg3, msg0);
-        SHA256_ROUND(msg1, i + 4);
-        msg2 = SHA256_MSG_UPDATE(msg2, msg3, msg0, msg1);
-        SHA256_ROUND(msg2, i + 8);
-        msg3 = SHA256_MSG_UPDATE(msg3, msg0, msg1, msg2);
-        SHA256_ROUND(msg3, i + 12);
+        wk = vaddq_u32(msg[3], vld1q_u32(&K[i + 12]));
+        temp_abcd = vsha256hq_u32(abcd, efgh, wk);
+        temp_efgh = vsha256h2q_u32(efgh, abcd, wk);
+        abcd = temp_abcd; efgh = temp_efgh;
+
+        if (i < 48) {
+            uint32x4_t temp_msg0 = vsha256su0q_u32(msg[0], msg[1]);
+            temp_msg0 = vsha256su1q_u32(temp_msg0, msg[2], msg[3]);
+            msg[0] = temp_msg0;
+
+            uint32x4_t temp_msg1 = vsha256su0q_u32(msg[1], msg[2]);
+            temp_msg1 = vsha256su1q_u32(temp_msg1, msg[3], msg[0]);
+            msg[1] = temp_msg1;
+
+            uint32x4_t temp_msg2 = vsha256su0q_u32(msg[2], msg[3]);
+            temp_msg2 = vsha256su1q_u32(temp_msg2, msg[0], msg[1]);
+            msg[2] = temp_msg2;
+
+            uint32x4_t temp_msg3 = vsha256su0q_u32(msg[3], msg[0]);
+            temp_msg3 = vsha256su1q_u32(temp_msg3, msg[1], msg[2]);
+            msg[3] = temp_msg3;
+        }
     }
 
-    uint32x4_t orig0 = vld1q_u32(&ctx->state[0]);
-    uint32x4_t orig1 = vld1q_u32(&ctx->state[4]);
-    vst1q_u32(&ctx->state[0], vaddq_u32(orig0, state0));
-    vst1q_u32(&ctx->state[4], vaddq_u32(orig1, state1));
-}
-#endif
-
-static void sha256_transform(SHA256_CTX *ctx, const uint8_t data[]) {
-#if defined(__ARM_FEATURE_CRYPTO)
-    sha256_transform_neon(ctx, data);
-#else
-    // 若不支援硬體加速，退回軟體迴圈運算
-    uint32_t a, b, c, d, e, f, g, h, i, j, t1, t2, m[64];
-    for (i = 0, j = 0; i < 16; ++i, j += 4)
-        m[i] = (data[j] << 24) | (data[j + 1] << 16) | (data[j + 2] << 8) | (data[j + 3]);
-    for ( ; i < 64; ++i)
-        m[i] = SIG1(m[i - 2]) + m[i - 7] + SIG0(m[i - 15]) + m[i - 16];
-    a = ctx->state[0]; b = ctx->state[1]; c = ctx->state[2]; d = ctx->state[3];
-    e = ctx->state[4]; f = ctx->state[5]; g = ctx->state[6]; h = ctx->state[7];
-    for (i = 0; i < 64; ++i) {
-        t1 = h + EP1(e) + CH(e, f, g) + k[i] + m[i];
-        t2 = EP0(a) + MAJ(a, b, c);
-        h = g; g = f; f = e; e = d + t1;
-        d = c; c = b; b = a; a = t1 + t2;
-    }
-    ctx->state[0] += a; ctx->state[1] += b; ctx->state[2] += c; ctx->state[3] += d;
-    ctx->state[4] += e; ctx->state[5] += f; ctx->state[6] += g; ctx->state[7] += h;
-#endif
+    abcd = vaddq_u32(abcd, abcd_save);
+    efgh = vaddq_u32(efgh, efgh_save);
+    vst1q_u32(state, abcd);
+    vst1q_u32(state + 4, efgh);
 }
 
 void sha256_init(SHA256_CTX *ctx) {
     ctx->datalen = 0;
     ctx->bitlen = 0;
-    ctx->state[0] = 0x6a09e667; ctx->state[1] = 0xbb67ae85;
-    ctx->state[2] = 0x3c6ef372; ctx->state[3] = 0xa54ff53a;
-    ctx->state[4] = 0x510e527f; ctx->state[5] = 0x9b05688c;
-    ctx->state[6] = 0x1f83d9ab; ctx->state[7] = 0x5be0cd19;
+    ctx->state[0] = 0x6a09e667;
+    ctx->state[1] = 0xbb67ae85;
+    ctx->state[2] = 0x3c6ef372;
+    ctx->state[3] = 0xa54ff53a;
+    ctx->state[4] = 0x510e527f;
+    ctx->state[5] = 0x9b05688c;
+    ctx->state[6] = 0x1f83d9ab;
+    ctx->state[7] = 0x5be0cd19;
 }
 
 void sha256_update(SHA256_CTX *ctx, const uint8_t data[], size_t len) {
-    uint32_t i;
-    for (i = 0; i < len; ++i) {
+    for (size_t i = 0; i < len; ++i) {
         ctx->data[ctx->datalen] = data[i];
         ctx->datalen++;
         if (ctx->datalen == 64) {
-            sha256_transform(ctx, ctx->data);
+            sha256_transform_armv8(ctx->state, ctx->data);
             ctx->bitlen += 512;
             ctx->datalen = 0;
         }
@@ -117,22 +107,28 @@ void sha256_update(SHA256_CTX *ctx, const uint8_t data[], size_t len) {
 }
 
 void sha256_final(SHA256_CTX *ctx, uint8_t hash[]) {
-    uint32_t i;
-    i = ctx->datalen;
-    ctx->data[i++] = 0x80;
+    uint32_t i = ctx->datalen;
     if (ctx->datalen < 56) {
-        memset(ctx->data + i, 0, 56 - i);
+        ctx->data[i++] = 0x80;
+        while (i < 56) ctx->data[i++] = 0x00;
     } else {
-        memset(ctx->data + i, 0, 64 - i);
-        sha256_transform(ctx, ctx->data);
+        ctx->data[i++] = 0x80;
+        while (i < 64) ctx->data[i++] = 0x00;
+        sha256_transform_armv8(ctx->state, ctx->data);
         memset(ctx->data, 0, 56);
     }
     ctx->bitlen += ctx->datalen * 8;
-    ctx->data[63] = ctx->bitlen; ctx->data[62] = ctx->bitlen >> 8;
-    ctx->data[61] = ctx->bitlen >> 16; ctx->data[60] = ctx->bitlen >> 24;
-    ctx->data[59] = ctx->bitlen >> 32; ctx->data[58] = ctx->bitlen >> 40;
-    ctx->data[57] = ctx->bitlen >> 48; ctx->data[56] = ctx->bitlen >> 56;
-    sha256_transform(ctx, ctx->data);
+    ctx->data[63] = ctx->bitlen;
+    ctx->data[62] = ctx->bitlen >> 8;
+    ctx->data[61] = ctx->bitlen >> 16;
+    ctx->data[60] = ctx->bitlen >> 24;
+    ctx->data[59] = ctx->bitlen >> 32;
+    ctx->data[58] = ctx->bitlen >> 40;
+    ctx->data[57] = ctx->bitlen >> 48;
+    ctx->data[56] = ctx->bitlen >> 56;
+    
+    sha256_transform_armv8(ctx->state, ctx->data);
+    
     for (i = 0; i < 4; ++i) {
         hash[i]      = (ctx->state[0] >> (24 - i * 8)) & 0x000000ff;
         hash[i + 4]  = (ctx->state[1] >> (24 - i * 8)) & 0x000000ff;
