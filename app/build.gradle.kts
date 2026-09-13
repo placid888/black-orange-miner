48f3412d8275cfbd603db31f500164558aa8be37
placid888 <placid888@gmail.com>
2026-09-13 19:33:45 +0800

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,8 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
 android {
     namespace = "com.manekiminer.app"
     compileSdk = 34
@@ -9,13 +14,11 @@ android {
         versionCode = 1
         versionName = "1.0"
         
-        // 鎖定 CPU 架構 (Poco M5 支援 arm64-v8a)
         ndk {
             abiFilters.add("arm64-v8a")
         }
     }
 
-    // 連結 CMakeLists.txt
     externalNativeBuild {
         cmake {
             path = file("src/main/cpp/CMakeLists.txt")
