8bc6b7f9a4c0e543d2ab0e52834ee043ebe3bd26
placid888 <placid888@gmail.com>
2026-09-18 00:49:15 +0800

    update

--- app/src/main/cpp/sha256.h
+++ app/src/main/cpp/sha256.h
@@ -1,27 +1,20 @@
-// app/src/main/cpp/sha256.h
 #ifndef SHA256_H
 #define SHA256_H
 
 #include <stddef.h>
 #include <stdint.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#define SHA256_BLOCK_SIZE 32
 
 typedef struct {
-    uint8_t data[64] __attribute__((aligned(16)));
-    uint32_t state[8] __attribute__((aligned(16)));
+    uint8_t data[64];
     uint32_t datalen;
     unsigned long long bitlen;
+    uint32_t state[8];
 } SHA256_CTX;
 
 void sha256_init(SHA256_CTX *ctx);
 void sha256_update(SHA256_CTX *ctx, const uint8_t data[], size_t len);
 void sha256_final(SHA256_CTX *ctx, uint8_t hash[]);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif // SHA256_H
