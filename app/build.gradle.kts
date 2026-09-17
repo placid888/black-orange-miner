f7ca1c13d8c9e0f4c4d80b7f896035e8c318ef6b
placid888 <placid888@gmail.com>
2026-09-18 00:33:05 +0800

    update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,56 +1,55 @@
-// app/build.gradle
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
 
 android {
-    namespace 'com.manekiminer.app'
-    compileSdk 34
+    namespace = "com.manekiminer.app"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId "com.manekiminer.app"
-        minSdk 26
-        targetSdk 34
-        versionCode 1
-        versionName "1.0"
+        applicationId = "com.manekiminer.app"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
 
         externalNativeBuild {
             cmake {
-                cppFlags "-std=c++17 -O3 -march=armv8-a+crypto"
+                cppFlags += "-std=c++17 -O3 -march=armv8-a+crypto"
             }
         }
         
         ndk {
-            abiFilters 'arm64-v8a'
+            abiFilters.add("arm64-v8a")
         }
     }
 
     buildTypes {
         release {
-            minifyEnabled false
-            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     
     externalNativeBuild {
         cmake {
-            path file('src/main/cpp/CMakeLists.txt')
-            version '3.22.1'
+            path = file("src/main/cpp/CMakeLists.txt")
+            version = "3.22.1"
         }
     }
     
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_1_8
-        targetCompatibility JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     
     kotlinOptions {
-        jvmTarget = '1.8'
+        jvmTarget = "1.8"
     }
 }
 
 dependencies {
-    implementation 'androidx.core:core-ktx:1.10.1'
-    implementation 'com.airbnb.android:lottie:6.0.0'
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("com.airbnb.android:lottie:6.0.0")
 }
