1d5e85e44cbd44490cb8de6ed0985bb7b11dc181
placid888 <placid888@gmail.com>
2026-09-18 00:18:34 +0800

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,44 +1,56 @@
+// app/build.gradle
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id 'com.android.application'
+    id 'org.jetbrains.kotlin.android'
 }
 
 android {
-    namespace = "com.manekiminer.app"
-    compileSdk = 34
+    namespace 'com.manekiminer.app'
+    compileSdk 34
 
     defaultConfig {
-        applicationId = "com.manekiminer.app"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        applicationId "com.manekiminer.app"
+        minSdk 26
+        targetSdk 34
+        versionCode 1
+        versionName "1.0"
+
+        externalNativeBuild {
+            cmake {
+                cppFlags "-std=c++17 -O3 -march=armv8-a+crypto"
+            }
+        }
         
         ndk {
-            abiFilters.add("arm64-v8a")
+            abiFilters 'arm64-v8a'
         }
     }
 
-    // 指定 Java 編譯版本為 17
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+    buildTypes {
+        release {
+            minifyEnabled false
+            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+        }
     }
-
-    // 指定 Kotlin 編譯版本為 17
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
+    
     externalNativeBuild {
         cmake {
-            path = file("src/main/cpp/CMakeLists.txt")
-            version = "3.22.1"
+            path file('src/main/cpp/CMakeLists.txt')
+            version '3.22.1'
         }
+    }
+    
+    compileOptions {
+        sourceCompatibility JavaVersion.VERSION_1_8
+        targetCompatibility JavaVersion.VERSION_1_8
+    }
+    
+    kotlinOptions {
+        jvmTarget = '1.8'
     }
 }
 
 dependencies {
-    implementation("com.airbnb.android:lottie:6.1.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation 'androidx.core:core-ktx:1.10.1'
+    implementation 'com.airbnb.android:lottie:6.0.0'
 }
